09cf19f352d37419748a6278004ebec88ee46695
Dare Day Labs <daredaylabs@gmail.com>
2026-09-26 16:23:42 +0000

    Enable PostHog Core Web Vitals telemetry

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -59,7 +59,7 @@ export function initAnalytics(customGaId?: string, customPosthogKey?: string) {
         capture_pageview: true,
         capture_pageleave: true,
         autocapture: true,
-        capture_performance: false,
+        capture_performance: true,
         session_recording: {
           recordCrossOriginIframes: false,
           maskAllInputs: false,
